909936bcc7a38198f17615069081844d3226d4e2
Aaron Echler <44124054+acechler@users.noreply.github.com>
2024-04-07 17:42:20 -0400

    undo keyhandler object

--- js/sketch.js
+++ js/sketch.js
@@ -1,5 +1,6 @@
 // reference: https://p5js.org/examples/objects-car-instances.html
 
+
 class Car {
   /* Constructor expects parameters for
   fill color, x and y coordinates that
@@ -11,18 +12,19 @@ class Car {
     this.width = 20;
     this.height = 10;
 
-
     this.color = cColor;
     this.doors = 4;
     this.isConvertible = false;
     this.speed = 0;
   }
 
-  start(speed) { // method expects parameter!
+  start(speed) {
+    // method expects parameter!
     this.speed = speed;
   }
 
-  display() { // method!
+  display() {
+    // method!
     fill(this.color);
     rect(this.x, this.y, this.width, this.height);
   }
@@ -34,15 +36,15 @@ class Car {
     } else if (this.x > width) {
       this.x = -20;
     }
-  
+
     // Corrected to use 'height' for canvas height
-    if (this.y < -20){
+    if (this.y < -20) {
       this.y = height;
-    } else if(this.y > height){
+    } else if (this.y > height) {
       this.y = -20;
     }
-  
-    switch(keyCode) {
+
+    switch (keyCode) {
       case 68: // D key
         this.x += this.speed;
         break;
@@ -57,37 +59,26 @@ class Car {
         break;
     }
   }
-  
 } //end class Car
-
 
 let rav4;
 
-
-
 function setup() {
   createCanvas(800, 800);
-  /* Construct the 3 Cars */
   //constructor expects cColor, x, y
   rav4 = new Car("silver", 100, 300);
 
-  
   console.log("rav4", rav4);
 
-  
   //call start methods of Car instances
   //the start method expects a number for speed
   rav4.start(2.3);
-
 }
 
 function draw() {
   background("beige");
-  
+
   rav4.display();
   rav4.move();
   //console.table(rav4);
-
 }
-
-
