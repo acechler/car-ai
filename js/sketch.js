af2535a3e5623b0a54607797fa53c00b1341f0c1
Aaron Echler <44124054+acechler@users.noreply.github.com>
2024-04-07 14:26:35 -0400

    Add p5 example car object

--- js/sketch.js
+++ js/sketch.js
@@ -1,32 +1,76 @@
 // reference: https://p5js.org/examples/objects-car-instances.html
-let x = 100,
-  y = 100,
-  angle1 = 0.0,
-  segLength = 50;
+
+class Car {
+  /* Constructor expects parameters for
+  fill color, x and y coordinates that
+  will be used to initialize class properties.
+  */
+  constructor(cColor, x, y) {
+    this.color = cColor;
+    this.doors = 4;
+    this.isConvertible = false;
+    this.x = x;
+    this.y = y;
+    this.speed = 0;
+  }
+
+  start(speed) { // method expects parameter!
+    this.speed = speed;
+  }
+
+  display() { // method!
+    fill(this.color);
+    rect(this.x, this.y, 20, 10);
+  }
+
+  move() { // method!
+    this.x += this.speed;
+    // Wrap x around boundaries
+    if (this.x < -20) {
+      this.x = width;
+    } else if (this.x > width) {
+      this.x = -20;
+    }
+  }
+} //end class Car
+
+
+let rav4;
+let charger;
+let nova;
+
+
 
 function setup() {
-  createCanvas(710, 400);
-  strokeWeight(20.0);
-  stroke(255, 100);
+  createCanvas(200, 400);
+  /* Construct the 3 Cars */
+  //constructor expects cColor, x, y
+  rav4 = new Car("silver", 100, 300);
+  charger = new Car("gold", 0, 200);  
+  nova = new Car("blue", 200, 100); 
+  nova.doors = 2; //update nova's doors property
+  
+  console.log("rav4", rav4);
+  console.log("charger", charger);
+  console.log("nova", nova);
+  
+  //call start methods of Car instances
+  //the start method expects a number for speed
+  rav4.start(2.3);
+  charger.start(-4);
+  nova.start(random(-1, 1));
 }
 
 function draw() {
-  background(0);
-
-  dx = mouseX - x;
-  dy = mouseY - y;
-  angle1 = atan2(dy, dx);
-  x = mouseX - cos(angle1) * segLength;
-  y = mouseY - sin(angle1) * segLength;
-
-  segment(x, y, angle1);
-  ellipse(x, y, 20, 20);
+  background("beige");
+  
+  //display and move all 3 Cars
+  rav4.display();
+  charger.display();
+  nova.display();
+  
+  rav4.move();
+  charger.move();
+  nova.move();
 }
 
-function segment(x, y, a) {
-  push();
-  translate(x, y);
-  rotate(a);
-  line(0, 0, segLength, 0);
-  pop();
-}
