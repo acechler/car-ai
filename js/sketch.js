54a8e98fe2455205534f8c186e6aec6d3cbe8801
Aaron Echler <44124054+acechler@users.noreply.github.com>
2024-04-07 11:27:43 -0400

    get rid of car for now

--- js/sketch.js
+++ js/sketch.js
@@ -1,10 +1,9 @@
-let car;
+
 
 function setup() {
   createCanvas(800, 800);
   rectMode(CENTER);
   angleMode(DEGREES);
-  car = new Car(100, 100, 25, 60); // Initialize car
 }
 
 function draw() {
@@ -12,7 +11,5 @@ function draw() {
 
   background(100); // Clear the canvas
   fill(255, 0, 0);
-  car.draw(); // Draw the car
-
 }
 
